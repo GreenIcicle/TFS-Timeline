2c68c887d7454fde1ac7f5b6169e58719842858b
Christian <chr.heger@gmail.com>
2012-04-07 18:37:08 +0200

    Fatored script into distinct files

---
+++ src/TfsTimeline/Scripts/tfs-timeline.common.js
@@ -0,0 +1,24 @@
+﻿var tfsTimeline = tfsTimeline || {};
+
+tfsTimeline.parseJsonDate = function (jsonDate) {
+    var offset = new Date().getTimezoneOffset() * 60000;
+    var parts = /\/Date\((-?\d+)([+-]\d{2})?(\d{2})?.*/.exec(jsonDate);
+
+    if (parts[2] == undefined) parts[2] = 0;
+
+    if (parts[3] == undefined) parts[3] = 0;
+
+    if (parts[2] == 0 && parts[3] == 0) offset = 0;
+
+    return new Date(+parts[1] + offset + parts[2] * 3600000 + parts[3] * 60000);
+};
+
+tfsTimeline.formatDurationAsMinutes = function (duration) {
+    var minutes = Math.floor(duration / 60);
+    var seconds = ("0" + (duration - minutes * 60)).slice(-2);
+    return minutes + ":" + seconds;
+};
+
+tfsTimeline.formatShortTime = function (time) {
+    return time.getHours() + ":" + ("0" + time.getMinutes()).slice(-2);
+};
---
+++ src/TfsTimeline/Scripts/tfs-timeline.viewmodel.build.js
@@ -0,0 +1,123 @@
+﻿var tfsTimeline = tfsTimeline || {};
+
+tfsTimeline.BuildsViewModel = function (serviceUrl) {
+    var self = this;
+
+    // Initial URL to retrieve data from
+    self.serviceUrl = serviceUrl;
+
+    // Observable array of build runs
+    self.builds = ko.observableArray([]);
+
+    // Indicates that data is still loading,
+    // initialize with true until data _has_been_ loaded.
+    self.isLoading = ko.observable(true);
+
+    // merges a build into the list of build view models
+    self.refreshBuild = function (newBuild) {
+        var currentBuild,
+            isNew = true;
+
+        for (var index = 0; index < self.builds().length; index++) {
+            currentBuild = self.builds()[index];
+            if (currentBuild.Uri() == newBuild.Uri) {
+                isNew = false;
+                self.applyToBuildViewModel(newBuild, currentBuild);
+            }
+        }
+
+        // create a new view model and insert it at the top of the list
+        if (isNew == true) {
+            self.builds.splice(0, 0, self.createBuildViewModel(newBuild));
+        }
+    };
+
+    // Copies all properties from a normal build to an observble viewmodel
+    self.applyToBuildViewModel = function (build, buildViewModel) {
+        for (var prop in build) {
+            buildViewModel[prop](build[prop]);
+        }
+    };
+
+    // Creates a new observable Build object
+    self.createBuildViewModel = function (build) {
+
+        var buildViewModel = {};
+
+        // Copy all properties from model to viewmodel
+        for (var prop in build) {
+            buildViewModel[prop] = ko.observable(build[prop]);
+        }
+
+        // display FxCop section only if there are either warnings or errors, 
+        // hide when there's nothing to show
+        buildViewModel.showFxCop = ko.computed(function () {
+            return ((buildViewModel.CodeAnalysiErrors() + buildViewModel.CodeAnalysisWarnings()) > 0);
+        });
+
+        // indicate that the build is currently running
+        buildViewModel.isRunning = ko.computed(function () {
+            return buildViewModel.Status() == "Running";
+        });
+
+        // Counts how long the build is running (in seconds)
+        buildViewModel.runDuration = ko.observable(0);
+
+        // Formats the information how long the build is running as m:ss
+        buildViewModel.runDurationMinutes = ko.computed(function () {
+            return tfsTimeline.formatDurationAsMinutes(self.runDuration());
+        });
+
+        // Formats the time that the build has been started at as hh:mm
+        buildViewModel.startedAtTime = ko.computed(function () {
+            var startedAt = tfsTimeline.parseJsonDate(buildViewModel.StartedAt());
+            return tfsTimeline.formatShortTime(startedAt);
+        });
+
+        return buildViewModel;
+    };
+
+    // after a new item is added to the list, fade it in
+    self.handleAfterAdd = function (domNode) {
+        $(domNode).fadeIn(1000);
+    };
+
+    // load data from the REST service 
+    self.refresh = function () {
+        $.ajax({
+            type: "POST",
+            url: self.serviceUrl,
+            data: "",
+            dataType: "json"
+        }).done(function (response) {
+
+            // merge each returned build item into the view model list
+            $.each(response.Builds, function (index, build) {
+                self.refreshBuild(build);
+            });
+
+            // update the service url with the URL to refresh the current build 
+            // list with
+            self.serviceUrl = response.Refresh;
+            self.isLoading(false);
+
+            // re-fetch data after 15 seconds
+            setTimeout('tfsTimeline.buildsViewModel.refresh();', 15000);
+        });
+    };
+
+    // As long as the build is running, update the build duration.
+    self.updateBuildDuration = function () {
+        var build;
+        for (var index = 0; index < self.builds().length; index++) {
+            build = self.builds()[index];
+            if (build.isRunning()) {
+                var startedAt = tfsTimeline.parseJsonDate(build.StartedAt());
+                var duration = Math.abs(new Date() - startedAt) * 1000;
+                build.runDuration(duration);
+            }
+        }
+        setTimeout('tfsTimeline.buildsViewModel.updateBuildDuration()', 1000);
+    };
+};
+
